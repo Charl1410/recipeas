53ebacbfddd2222e2e3534fa9b0ef6e5f92ddd6d
charlotte <charlottegriffinn@hotmail.com>
2026-06-08 10:54:24 +0100

    fixing path issue for types

--- src/app/components/Cards/RecipeCard.tsx
+++ src/app/components/Cards/RecipeCard.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { FaHeart } from "react-icons/fa";
 import StarRating from '../StarRating';
-import { RecipeData } from '../../types';
+import { RecipeData } from '../../types/index';
 import ViewRecipeButton from '../Buttons/ViewRecipeButton';
 import Image from 'next/image';
 
